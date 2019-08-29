9e1e8e73e207d3f986b034eeb0f0792088f2ad36
fuxiangyang <fuxiangyang945@126.com>
2019-08-29 18:12:15 +0800

    vuepress测试

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    base: '/tech-snail/',
+    title: '蜗牛🐌不牛',
+    description: '技术方面的沉淀，积累',
+    themeConfig: {
+        nav: [
+        	{
+	            text: '指南',
+	            link: '/guide/'
+        	},
+        	{
+	            text: '配置参考',
+	            link: '/config/'
+        	},
+        	{
+	            text: 'GitHub',
+	            link: 'https://github.com/fuxy1011/tech-snail'
+        	}
+        ],
+        sidebarDepth: 2,
+        sidebar: {
+            '/guide/': [{
+                title: '指南',
+                collapsable: false,
+                children: [
+                '介绍', 
+                '起步', 
+                ['配置', 'Explicit link text']]
+            }],
+            '/config/': [{
+                title: '配置参考',
+                collapsable: false,
+                children: [
+                '基本配置', 
+                '主题化', 
+                ['Markdown', 'Markdown']]
+            }]
+        }
+    }
+}
